4f3a6facc124517cd226721eb5a7c37b009ca1b0
Choiyoungdok <rhismasyahrulputra11@gmail.com>
2025-07-04 11:43:34 +0700

    update deployment

--- vite.config.js
+++ vite.config.js
@@ -24,7 +24,7 @@ export default defineConfig({
         theme_color: "#ffffff",
         background_color: "#ffffff",
         display: "standalone",
-        start_url: "/",
+        start_url: "/Siakku-Clone/",
         icons: [
           {
             src: "pwa-192x192.png",
@@ -46,5 +46,5 @@ export default defineConfig({
       },
     }),
   ],
-  base: "/",
+  base: "/Siakku-Clone/",
 });
